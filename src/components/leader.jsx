73fd292b72d46f0bb3a3d03d5fdde888c764bc66
Satyam3002 <satyamlaheri3@gmail.com>
2024-09-25 21:57:21 +0530

    update

--- src/components/leader.jsx
+++ src/components/leader.jsx
@@ -14,8 +14,8 @@ export default function Leader() {
 
       <div className="flex flex-col lg:pl-60 gap-y-8">
         <div className="flex flex-col lg:flex-row gap-y-4 lg:gap-x-6">
-          <div className="w-full lg:w-3/12 h-32 py-2 rounded-full border"></div>
-          <div className="flex flex-row w-full lg:w-1/5 border  rounded-full overflow-hidden">
+          <div className="w-full lg:w-2/5 h-32 py-2 rounded-full border"></div>
+          <div className="flex flex-row w-full lg:w-1/3 border  rounded-full overflow-hidden">
             <div className="w-1/2 bg-blue-500 h-full"></div>
             <div className="flex flex-col gap-y-1 bg-blue-700 w-1/2 p-2">
               <p>Cody Fisher</p>
@@ -27,16 +27,16 @@ export default function Leader() {
                   src="./insta.png"
                   alt="Instagram icon"
                 />
-                <img className="w-8 h-8" src="./be.png" alt="LinkedIn icon" />
+                <img className="w-8 h-8" src="./Be.png" alt="LinkedIn icon" />
               </div>
             </div>
           </div>
-          <div className="w-full lg:w-3/12 h-32 py-2 rounded-full border"></div>
+          <div className="w-full lg:w-2/5 h-32 py-2 rounded-full border"></div>
         </div>
 
         <div className="flex flex-col lg:flex-row gap-y-4 lg:gap-x-6">
-          <div className="w-full lg:w-3/12 h-32 py-2 rounded-full border"></div>
-          <div className="flex flex-row w-full lg:w-1/5 rounded-full border overflow-hidden">
+          <div className="w-full lg:w-2/5 h-32 py-2 rounded-full border"></div>
+          <div className="flex flex-row w-full lg:w-1/3 rounded-full border overflow-hidden">
             <div className="w-1/2 bg-zinc-800 h-full"></div>
             <div className="flex flex-col gap-y-1 bg-zinc-900 w-1/2 p-2">
               <p>Cody Fisher</p>
@@ -48,18 +48,18 @@ export default function Leader() {
                   src="./insta.png"
                   alt="Instagram icon"
                 />
-                <img className="w-8 h-8" src="./be.png" alt="LinkedIn icon" />
+                <img className="w-8 h-8" src="./Be.png" alt="" />
               </div>
             </div>
           </div>
-          <div className="w-full lg:w-3/12 h-32 py-2 rounded-full border"></div>
+          <div className="w-full lg:w-3/12 h-32 py-2  rounded-full border"></div>
         </div>
 
         <div className="flex flex-col lg:flex-row gap-y-4 lg:gap-x-6">
-          <div className="w-full lg:w-3/12 h-32 py-2 rounded-full border"></div>
-          <div className="flex flex-row w-full lg:w-1/5 rounded-full border overflow-hidden">
+          <div className="w-full lg:w-2/5 h-32 py-2 rounded-full border"></div>
+          <div className="flex flex-row w-full lg:w-1/3 border  rounded-full overflow-hidden">
             <div className="w-1/2 bg-zinc-800 h-full"></div>
-            <div className="flex flex-col gap-y-1 bg-zinc-900 w-1/2 p-2">
+            <div className="flex flex-col gap-y-1 bg-zinc-900  w-1/2 p-2">
               <p>Cody Fisher</p>
               <p className="text-sm">CEO Founder</p>
               <div className="flex gap-x-1">
@@ -69,11 +69,11 @@ export default function Leader() {
                   src="./insta.png"
                   alt="Instagram icon"
                 />
-                <img className="w-8 h-8" src="./be.png" alt="LinkedIn icon" />
+                <img className="w-8 h-8" src="./Be.png" alt="LinkedIn icon" />
               </div>
             </div>
           </div>
-          <div className="w-full lg:w-3/12 h-32 py-2 rounded-full border"></div>
+          <div className="w-full lg:w-2/5 h-32 py-2 rounded-full border"></div>
         </div>
       </div>
     </div>
